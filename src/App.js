3b692b1b22c5887694a511a0d631341413d7613a
Nashir Jamali <nashirjamali@gmail.com>
2022-08-24 17:41:42 +0700

    wip landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import'assets/scss/style.scss'
+import "./assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LandingPage />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -5,11 +5,11 @@ import propTypes from 'prop-types';
 export default function Button(props) {
   const className = [propTypes.className];
 
-  if (props.isPrimary) className.push['btn-primary'];
-  if (props.isSmall) className.push['btn-sm'];
-  if (props.isLarge) className.push['btn-lg'];
-  if (props.isBlock) className.push['btn-block'];
-  if (props.hasShadow) className.push['btn-shadow'];
+  if (props.isPrimary) className.push('btn-primary');
+  if (props.isSmall) className.push('btn-sm');
+  if (props.isLarge) className.push('btn-lg');
+  if (props.isBlock) className.push('btn-block');
+  if (props.hasShadow) className.push('btn-shadow');
 
   const onClick = () => {
     if (props.onClick) props.onClick();
