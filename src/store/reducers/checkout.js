26d5b766465beea1dec125b1859d361b05d2188b
Nashir Jamali <nashirjamali@gmail.com>
2022-11-23 13:27:33 +0700

    wip redux

---
+++ src/store/reducers/checkout.js
@@ -0,0 +1,12 @@
+import { CHECKOUT_BOOKING } from "../types";
+
+const intialState = null;
+
+export default function (state = intialState, action) {
+  switch (action.type) {
+    case CHECKOUT_BOOKING:
+      return action.payload;
+    default:
+      return state;
+  }
+}
